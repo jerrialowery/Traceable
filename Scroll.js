b126b70c5eff81b10d7735d8f852271f6916a601
Jerria L <jlowery@mica.edu>
2018-05-09 02:31:47 -0400

    Traceable New

--- Scroll.js
+++ Scroll.js
@@ -55,3 +55,34 @@ $(function() {
     }
   });
 });
+
+document.cookie = "name=Storing";
+document.cookie = "name=Hello! Storing Cookies Now";
+
+var cookieValue = "Hello"
+function alertCookieValue() {
+  alert("Hello! Thanks for clicking, and by the way your responses and personal data are being recorded right now.");
+
+}
+
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+}
+
+
+function myFunction(imgs) {
+// Get the expanded image
+var expandImg = document.getElementById("expandedImg");
+// Get the image text
+var imgText = document.getElementById("imgtext");
+// Use the same src in the expanded image as the image being clicked on from the grid
+expandImg.src = imgs.src;
+// Use the value of the alt attribute of the clickable image as text inside the expanded image
+imgText.innerHTML = imgs.alt;
+// Show the container element (hidden with CSS)
+expandImg.parentElement.style.display = "block";
+}
